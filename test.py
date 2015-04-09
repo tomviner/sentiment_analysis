c9e716679d09f1b8567192d8c522fab7db730e2b
TomV <tom@viner.tv>
2015-04-09 21:16:57 +0100

    write actual test

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
-
 from analyser import sort_articles
 
-articles = (
-    "worst bad awful bad bad bad thing. but worse",
-    "really good, nice things and a pixie"
-)
 
-sorted_articles = sort_articles(articles)
+def test_sorting():
+    articles = [
+        "worst bad awful bad bad bad thing. but worse",
+        "dog",
+        "cat",
+        "neutral",
+        "really good, nice things and a pixie",
+        "happy awesome amazing fabulous ice cream and chocolate",
+    ]
 
-assert sorted_articles == articles
+    sorted_articles = sort_articles(articles)
+
+    assert sorted_articles == articles
